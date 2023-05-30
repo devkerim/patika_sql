e7324a7d2c4b4237bc8f1ed7da4461fe4fa8e596
devkerim <70816716+devkerim@users.noreply.github.com>
2023-05-30 22:19:08 +0300

    Add files via upload

---
+++ HW10.sql
@@ -0,0 +1,8 @@
+--1
+SELECT city.city,country.country FROM city LEFT JOIN country ON country.country_id=city.country_id;
+
+--2
+SELECT customer.first_name,customer.last_name,payment.payment_id FROM customer RIGHT JOIN payment ON payment.customer_id=customer.customer_id; 
+
+--3
+SELECT customer.first_name,customer.last_name,rental.rental_id FROM customer FULL JOIN rental ON rental.customer_id=customer.customer_id;
